d4fa02a2deb42e4f31d90f45541cb00798e5dd4c
Stephen Parker <stephenparkhum@outlook.com>
2019-06-09 19:17:53 -0700

    Adding function stubs and user stories

---
+++ main.js
@@ -0,0 +1,34 @@
+// :MUSIC QUERY: NOTES
+/*
+
+******* STEPS ******* 
+1. Line out user stories 
+2. Create Function Stubs
+3. Add questions to 'questions.js'
+3. Implement functions
+
+
+******* USER STORES ******* 
+1. User can start a new quiz 
+2. Question is displayed, with options
+3. User Submits answer
+4. If answer correct/incorrect, the correct answer will display green, and the incorrect will display red
+5. New Quiz button, which will restart the quiz
+*/
+
+/* 
+newQuiz() will show a dialog that asks if the user wants to start a new quiz
+Once clicked, it'll show the question + answer. 
+Ultimately this will 'hide; the new quiz button, as well as 'show' the questions
+*/
+function newQuiz() {}
+
+/* 
+question() will display each question, as well as the options
+*/
+function question() {}
+
+/* 
+questionOptions() displays the answer options for each question 
+*/
+function questionOptions() {}
