36492fc597a526c138a6687eaacbd16c90ff2893
Stephen Parker <stephenparkhum@outlook.com>
2019-06-12 12:23:28 -0700

    Removing old code and comments

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ Ultimately this will 'hide; the new quiz button, as well as 'show' the questions
 */
 
 function newQuiz(num) {
-  $(document).on("click", ".new-quiz button", () => {
+  $(document).on("click", ".new-quiz", () => {
     question(num);
     formInit();
     // optionsInit(num);
@@ -118,36 +118,6 @@ function removeOptions() {
   $('form').find("button .submit-button").remove();
 }
 
-// function formOptions(num) {
-//   
-//   let optionCount = 0;
-//   num = questionNum;
-//   let qNum = 0;
-
-//   // questions[0].options[0][`${optionList[0]}`]
-
-//   for (let i = 0; i < questions[0].options.length; i++) {
-//     $('form').html(inputOptions(i, optionList, optionCount));
-//     console.log(questions[num].options[num][`${optionList[0]}`]);
-//   }
-    
-  // let formHtml = `
-  //   <input
-  //     type="radio"
-  //     name="option"
-  //     id="option1"
-  //     value="${questions[num].options[qNum].option_1}"
-  //   />${questions[num].options[qNum].option_1}<br />
-  //   <input type="radio" name="option" id="option2" value="${questions[num].options[qNum + 1].option_2}" />${questions[num].options[qNum + 1].option_2}<br />
-  //   <input type="radio" name="option" id="option3" value="${questions[num].options[qNum + 2].option_3}" />${questions[num].options[qNum + 2].option_3}<br />
-  //   <input
-  //     type="radio"
-  //     name="option"
-  //     id="option4"
-  //     value="${questions[num].options[qNum + 3].option_4}"/>${questions[num].options[qNum + 3].option_4}<br />
-  //   <button class="submit-button">Submit</button>`;
-  // $(".option-form").html(formHtml);
-
 /* 
 question() will display each question, as well as the options
 Within each question there will be a display of which question (out of 10) it is.
@@ -165,6 +135,7 @@ function nextBtn() {
       questionState++;
     } else {
       $('.next-btn').text('Complete Quiz').addClass('complete-quiz ');
+      resultsPage();
 
     }
     
@@ -184,7 +155,7 @@ This will be stored in an array, and display in the question box after the quiz 
 -- Storing the points will be counted as 'correct' or 'incorrect'
 */
 
-function resultsPage(result) {
+function resultsPage() {
   $(document).on('click', '.complete-quiz', () => {
     $('.question p:first').text('Congratulations! You have finished the quiz!');
     $('.question').append('<h4>Your score is 3 out of 10! Better luck next time!</h4>');
