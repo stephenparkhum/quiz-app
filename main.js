606e4e08fe33a20d3d1cc390602522efa08f7b64
Stephen Parker <stephenparkhum@outlook.com>
2019-06-17 18:02:03 -0700

    Updating JS classes for a smoother UX

--- main.js
+++ main.js
@@ -13,8 +13,8 @@
 
 
 // USERS SCORES
-let userScore = [];
-let questionNum = userScore.length;
+// let userScore = [];
+// let questionNum = userScore.length;
 
 class Quiz {
   constructor(questions) {
@@ -23,15 +23,179 @@ class Quiz {
     this.questions = questions;
   }
 
-  canSelect() {
-    return (this.questions > this.userScore);
+  // Questions
+  question() {
+    if (this.questionNum <= 9) {
+      $("p[class='question-text']").text(`${questions[this.questionNum].question}`);
+      console.log(questionCount(this.questionNum));
+    }
   }
 
-  startPage() {
-    
+  questionIncrement() {
+    this.question();
+    this.removeOptions();
+    this.newOptions();
+    console.log('question inc. is working');
   }
+
+  // OPTIONS
+  formInit() {
+    let initialForm = `<div class="options">
+    <form action="" id="option-form">
+    </form>
+  </div>
+  <button class="next-btn">Next</button>`;
+    $("main").append(initialForm);
+    $('.next-btn').hide();
+    this.newOptions();
+  }
+
+  newOptions() {
+    let num = this.questionNum;
+    let labelsAndInputs;
+    if (num <= 9) {
+      for (let i = 0; i < 4; i++) {
+        labelsAndInputs = `<input type="button" name="option" id="${i + 1}" value="${questions[num].options[i].option}" class="option-empty" required/>`;
+        $('form').append(labelsAndInputs);
+      }
+    }
+  }
+
+  optionValidate() {
+    let num = this.questionNum;
+    $(document).on('click', 'input', function (event) {
+      let selection = event.target.getAttribute('value');
+      let answer = questions[num]['answer'];
+      if (selection == answer) {
+        $(event.target).removeClass('option-empty');
+        $(event.target).addClass('correct');
+        $(event.target).attr('checked', "checked");
+        correctScore();
+        if (num <= 8) {
+          $('.next-btn').show();
+          num++;
+        } else {
+          $('.next-btn').show();
+          $('form .next-btn').removeClass('next-btn');
+          $('main button').text('Complete Quiz').addClass('complete-quiz');
+        }
+      } else {
+        wrongScore();
+        $(event.target).removeClass('option-empty');
+        $(event.target).addClass('incorrect');
+        num++;
+        $('.next-btn').show();
+      }
+  
+    });
+  }
+
+  removeOptions() {
+    let optionCount = 4;
+    for (let i = 0; i < optionCount; i++) {
+      $('form').find('input').remove();
+      $('form').find('label').remove();
+      $('form').find(".submit-button").remove();
+    }
+  }
+
+
+  newQuiz() {
+    $(document).on("click", ".new-quiz", () => {
+      this.question();
+      this.formInit();
+      $(".new-quiz button").remove();
+    });
+  }
+
+  testFunc() {
+    console.log(this.userScore);
+  }
+
+  // BUTTONS
+  nextBtn(num) {
+    $(document).on("click", ".next-btn", () => {
+      if (num <= 9) {
+        questionIncrement(num);
+        num++;
+        $('.next-btn').hide();
+        console.log(num);
+      } else {
+        resultsPage();
+      }
+    });
+  }
+
+  buttonsMaster(num) {
+    nextBtn(num);
+  }
+
+  // QUIZ RESULTS
+  calcResults(results) {
+    let userRightCount = 0;
+    for (let i = 0; i < results.length; i++) {
+      if (results[i] === true) {
+        userRightCount++;
+      }
+    }
+    return userRightCount;
+  }
+
+  correctScore() {
+    this.userScore.push(true);
+  }
+  
+  wrongScore() {
+    this.userScore.push(false);
+  }
+
 }
 
+
+
+function quizInit() {
+  startPage();
+}
+
+/* 
+newQuiz() will show a dialog that asks if the user wants to start a new quiz
+Once clicked, it'll show the question + answer. 
+Ultimately this will 'hide; the new quiz button, as well as 'show' the questions
+*/
+
+
+
+/* 
+question() will display each question, as well as the options
+Within each question there will be a display of which question (out of 10) it is.
+*/
+
+/* 
+questionCount() will display which question (out of 10) it is.
+*/
+
+/* 
+optionForm() will display the set of options the user has to pick from
+*/
+
+/* 
+The User's Score will be stored as a '1' for correct, and '0' for incorrect. 
+*/
+
+
+
+
+
+
+
+// RESULTS
+/* 
+results() displays the completed scores of how many questions were right and how many were wrong.
+This will be stored in an array, and display in the question box after the quiz has been completed.
+-- Storing the points will be counted as 'correct' or 'incorrect'
+*/
+
+// HTML FUNCTIONS
 function startPage() {
   $("main").append(
     '<div class="question"></div>'
@@ -45,180 +209,6 @@ function startPage() {
   );
 }
 
-function quizInit() {
-  startPage();
-}
-
-/* 
-newQuiz() will show a dialog that asks if the user wants to start a new quiz
-Once clicked, it'll show the question + answer. 
-Ultimately this will 'hide; the new quiz button, as well as 'show' the questions
-*/
-
-function newQuiz(num) {
-  $(document).on("click", ".new-quiz", () => {
-    question(num);
-    formInit(num);
-    $(".new-quiz button").remove();
-  });
-}
-
-/* 
-question() will display each question, as well as the options
-Within each question there will be a display of which question (out of 10) it is.
-*/
-function question(num) {
-  num = userScore.length;
-  if (num <= 9) {
-    $("p[class='question-text']").text(`${questions[num].question}`);
-    questionCount(num);
-  }
-
-}
-
-/* 
-questionCount() will display which question (out of 10) it is.
-*/
-function questionCount(num) {
-  $(".question-count").text(`Question ${num + 1} of 10`);
-}
-
-/* 
-optionForm() will display the set of options the user has to pick from
-*/
-function formInit(num) {
-  let initialForm = `<div class="options">
-  <form action="" id="option-form">
-  </form>
-</div>
-<button class="next-btn">Next</button>`;
-  $("main").append(initialForm);
-  $('.next-btn').hide();
-  newOptions(num);
-}
-
-function newOptions(num) {
-  num = userScore.length;
-  let labelsAndInputs;
-  if (num <= 9) {
-    for (let i = 0; i < 4; i++) {
-      labelsAndInputs = `<input type="button" name="option" id="${i + 1}" value="${questions[num].options[i].option}" class="option-empty" required/>`;
-      $('form').append(labelsAndInputs);
-    }
-  }
-}
-
-function optionValidate(num) {
-  num = 0;
-  $(document).on('click', 'input', function (event) {
-    let selection = event.target.getAttribute('value');
-    let answer = questions[num]['answer'];
-    if (selection == answer) {
-      $(event.target).removeClass('option-empty');
-      $(event.target).addClass('correct');
-      $(event.target).attr('checked', "checked");
-      correctScore();
-      if (num <= 8) {
-        $('.next-btn').show();
-        num++;
-      } else {
-        $('.next-btn').show();
-        $('form .next-btn').removeClass('next-btn');
-        $('main button').text('Complete Quiz').addClass('complete-quiz');
-      }
-    } else {
-      wrongScore();
-      $(event.target).removeClass('option-empty');
-      $(event.target).addClass('incorrect');
-      num++;
-      $('.next-btn').show();
-    }
-
-  });
-}
-
-/* 
-The User's Score will be stored as a '1' for correct, and '0' for incorrect. 
-*/
-function correctScore() {
-  userScore.push(1);
-}
-
-function wrongScore() {
-  userScore.push(0);
-}
-
-function removeOptions() {
-  let optionCount = 4;
-  for (let i = 0; i < optionCount; i++) {
-    $('form').find('input').remove();
-    $('form').find('label').remove();
-    $('form').find(".submit-button").remove();
-  }
-}
-
-function questionIncrement(num) {
-  question(num);
-  removeOptions(num);
-  newOptions(num);
-  console.log('question inc. is working');
-}
-
-// BUTTONS
-const buttonsMaster = (num) => {
-  nextBtn(num);
-};
-
-function nextBtn(num) {
-  $(document).on("click", ".next-btn", () => {
-    if (num <= 9) {
-      questionIncrement(num);
-      num++;
-      $('.next-btn').hide();
-      console.log(num);
-    } else {
-      resultsPage();
-    }
-  });
-}
-
-
-// function tryAgain(num) {
-//   $(document).on('click', '.js-try-again', () => {
-//     $('.question').find('h4').remove();
-//     userScore = [];
-//     resetQuiz(num);
-//   });
-// }
-
-function resetQuiz(num) {
-  newQuiz(num);
-  optionValidate(num);
-}
-
-
-// MASTERS
-function questionMaster(num) {}
-
-
-
-
-// RESULTS
-/* 
-results() displays the completed scores of how many questions were right and how many were wrong.
-This will be stored in an array, and display in the question box after the quiz has been completed.
--- Storing the points will be counted as 'correct' or 'incorrect'
-*/
-
-function calcResults(results) {
-  let userRightCount = 0;
-  for (let i = 0; i < results.length; i++) {
-    if (results[i] === 1) {
-      userRightCount++;
-    }
-  }
-  return userRightCount;
-}
 
 function resultsPage(results) {
   $(document).on('click', '.complete-quiz', () => {
@@ -247,11 +237,14 @@ function resultsPage(results) {
 /* 
 runQuiz() will include all of the other needed functions to run
 */
-function runQuiz(num) {
-  quizInit();
-  resetQuiz(num);
-  resultsPage(userScore);
-  buttonsMaster(num);
+
+function runQuiz() {
+  startPage();
+  const quizInitiate = new Quiz();
+  quizInitiate.newQuiz();
+  quizInitiate.testFunc();
+  quizInitiate.question();
+  resultsPage();
 }
 
-runQuiz(userScore.length);
+runQuiz();
